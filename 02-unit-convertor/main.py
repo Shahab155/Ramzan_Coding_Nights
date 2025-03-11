919d557dabe3f52da79cd8c85d93bb28d8bfa9ec
shahab2015 <166250566+shahab2015@users.noreply.github.com>
2025-03-11 06:19:09 +0500

    add unit-convertor

--- 02-unit-convertor/main.py
+++ 02-unit-convertor/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from 02-unit-convertor!")
+import streamlit as st
 
+def convert_unit(value, unit_from, unit_to):
+    conversions = {
+      "meter_kilometer": 0.001,
+      "kilometer_meter": 1000,
+      "gram_kilogram": 0.001,
+      "kilogram_gram": 1000
+    }
+    
+    key = f"{unit_from}_{unit_to}"
+    if key in conversions:
+        conversion = conversions[key]
+        return value * conversion
+    
+st.title("🌎Unit Convertor")    
 
-if __name__ == "__main__":
-    main()
+value = st.number_input("Enter the value", min_value=1.0, step=1.0)
+
+unit_from = st.selectbox("Convert From:", ["meter","kilometer","gram","kilogram"])
+
+unit_to = st.selectbox("Convert to:", ["meter","kilometer","gram","kilogram"])
+
+if st.button("Convert"):
+    result = convert_unit(value, unit_from, unit_to)
+    st.success(f"{value}_{unit_from} = {result}_{unit_to}")
